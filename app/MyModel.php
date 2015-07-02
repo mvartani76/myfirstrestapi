bda02511cd5fb777573e91b02980f11bac1d0116
Mike Vartanian <mike.vartanian@gmail.com>
2015-07-02 11:48:52 -0400

    *.*

---
+++ app/MyModel.php
@@ -0,0 +1,12 @@
+<?php namespace App
+
+use Illuminate\Database\Eloquent\Model;
+
+class MyModel extends Model
+{
+	protected $fillable = ['name','phone','secretAttribute','password'];
+
+	protected $hidden = ['secretAttribute','password'];
+}
+
+?>
